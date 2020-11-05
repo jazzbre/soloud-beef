544432dc6bec18226eac91f1dffffd23f7ea3169
Aleš Mlakar <ales.ambits@gmail.com>
2020-11-05 08:36:14 +0100

    FIX: Debug configuration added

--- build_windows_vs2019.cmd
+++ build_windows_vs2019.cmd
@@ -16,7 +16,15 @@ set "VISUALSTUDIO19PATH=c:\Program Files (x86)\Microsoft Visual Studio\2019\Comm
 )
 
 IF EXIST "%VISUALSTUDIO19PATH%" (
-echo Building BGFX in Visual Studio 2019
+echo Building SoLoud Debug in Visual Studio 2019
+"%VISUALSTUDIO19PATH%\devenv" ".build\projects\vs2019\soloud.sln" /Build "Debug|x64"
+echo ErrorLevel:%ERRORLEVEL%
+IF %ERRORLEVEL% EQU 0 (
+   echo Build successful!
+) else (
+   echo Build failed!
+)
+echo Building SoLoud Release in Visual Studio 2019
 "%VISUALSTUDIO19PATH%\devenv" ".build\projects\vs2019\soloud.sln" /Build "Release|x64"
 echo ErrorLevel:%ERRORLEVEL%
 IF %ERRORLEVEL% EQU 0 (
