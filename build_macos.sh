ba4b85f22c7ae6fb6536755b5f22c5d5743e900f
Aleš Mlakar <ales.ambits@gmail.com>
2021-01-03 11:26:23 +0100

    UPDATE: Latest submodules + macOS x64/arm64 split + soloud fork used

--- build_macos.sh
+++ build_macos.sh
@@ -4,6 +4,6 @@ if [ -d $TESTSDL2PATH ]; then
    echo Setting SDL2 path to $TESTSDL2PATH!
    export SDL2PATH=$TESTSDL2PATH
 fi
-./submodules/bx/tools/bin/darwin/genie --gcc=osx gmake
-make -C .build/projects/gmake-osx config=debug64 -j 8
-make -C .build/projects/gmake-osx config=release64 -j 8
+./submodules/bx/tools/bin/darwin/genie --gcc=osx-x64 gmake
+make -C .build/projects/gmake-osx-x64 config=debug -j 8
+make -C .build/projects/gmake-osx-x64 config=release -j 8
